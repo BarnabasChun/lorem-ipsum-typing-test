f3967dfe39dd78b88787ca90b81795ae581df9af
Barnabas Chun <chun.barnabas@gmail.com>
2018-01-23 15:13:34 -0500

    refactored accuracy expression

--- dev/scripts/modules/displayMetrics.js
+++ dev/scripts/modules/displayMetrics.js
@@ -4,15 +4,8 @@ import calcWPM from './calcWPM';
 
 export default function displayMetrics() {
   let accuracy;
-
-  if (state.currentIndex === 0) {
-    // if no words have been typed current index = 0
-    // cannot divide by 0 so
-    accuracy = 0;
-  } else {
-    // number of correct words / words typed 
-    accuracy = calcAccuracy(state.correct, state.currentIndex);
-  }
+  /* if no words have been typed current index = 0; and you can't divide by 0 so return 0 if nothing valid returns from calcAccuracy*/
+  accuracy = calcAccuracy(state.correct, state.currentIndex) || 0;
   // number of words typed / min
   const wpm = calcWPM(state.correct, state.seconds);
   document.querySelector('.wpm').innerText = `WPM: ${wpm}`;
